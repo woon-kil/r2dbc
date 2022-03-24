1c13a6b69cf283e0abf1d8beb54fd1d5e3449129
woonkil <woonkil70@gmail.com>
2022-03-24 15:49:28 +0900

    first commit

--- src/main/java/com/example/accessingdatar2dbc/web/CustomerController.java
+++ src/main/java/com/example/accessingdatar2dbc/web/CustomerController.java
@@ -2,10 +2,15 @@ package com.example.accessingdatar2dbc.web;
 
 import com.example.accessingdatar2dbc.Customer;
 import com.example.accessingdatar2dbc.CustomerRepository;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.awt.*;
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -18,7 +23,12 @@ public class CustomerController {
 
     @GetMapping("/flux")
     public Flux<Integer> flux(){
-        return Flux.just(1,2,3,4,5);
+        return Flux.just(1,2,3,4,5).delayElements(Duration.ofSeconds(1)).log();
+    }
+
+    @GetMapping(value = "/fluxstream",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> fluxstream(){
+        return Flux.just(1,2,3,4,5).delayElements(Duration.ofSeconds(3)).log();
     }
 
     @GetMapping("/customer")
